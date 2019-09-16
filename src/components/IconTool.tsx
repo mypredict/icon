f50b32f70df4a453f53a5edf26cff4ae505145c6
guolj <mypredicts@gmail.com>
2019-09-16 23:34:37 +0800

    .

--- src/components/IconTool.tsx
+++ src/components/IconTool.tsx
@@ -367,7 +367,7 @@ const IconTool = (props: Props) => {
           }
         </button>
         {
-          !props.bulkEdit && (
+          props.bulkEdit && (
             <Checkbox
               label="全选"
               labelStyle={{
